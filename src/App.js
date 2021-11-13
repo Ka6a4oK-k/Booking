02c8c72e20ae1db9bc36c94bec775a3b9f556662
Ka6a4oK-k <kyb4uk@gmail.com>
2021-11-13 17:07:33 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Login } from './login';
+import { Register } from './register'
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
